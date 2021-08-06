43d27cd396f19f13144ddc485a7665d922fcdb85
RadianceOkuzor <75043377+RadianceOkuzor@users.noreply.github.com>
2021-08-06 00:33:48 -0500

    init

---
+++ EchoChamber/src/firebase.js
@@ -0,0 +1,87 @@
+// import firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore' ;
+import 'firebase/analytics'
+import "firebase/auth";
+
+
+// import 'firebase/firestore';
+// const firebase = require("firebase/firebase");
+// // Required for side-effects
+// require("firebase/firestore");
+// require("firebase/auth");
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApUKFGeW05Iv9-7JFSSnIhCzNZXHhBxb0",
+  authDomain: "trash-talkers.firebaseapp.com",
+  databaseURL: "https://trash-talkers.firebaseio.com",
+  projectId: "trash-talkers",
+  storageBucket: "trash-talkers.appspot.com",
+  messagingSenderId: "17956094321",
+  appId: "1:17956094321:web:38005c16137bf31222f6d0",
+  measurementId: "G-GR572FQHQQ"
+};
+
+// Initialize Firebase
+export const app = firebase.initializeApp(firebaseConfig);
+firebase.analytics()
+
+
+
+
+export const firestore = app.firestore();
+export const auth = app.auth();
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const signInWithGoogle = () => auth.signInWithRedirect(provider);
+export const signOut = () => auth.signOut();
+
+// Signup form into Creating a User
+export const createUserProfileDocument = async (user, additionalData) =>{
+  if (!user) return;
+
+  const userRef = firestore.doc(`users/${user.id}`)
+
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exits){
+    const {displayName, email, photoUrl} = user;
+    const createdAt = new Date();
+      try{
+        await userRef.set({
+displayName,
+email,
+photoUrl,
+createdAt,
+...additionalData,
+        })
+      } catch (error){
+      console.error('Error Creating User', error.message)
+      }
+  }
+
+  return getUserDocument(user.uid);
+};
+
+export const getUserDocument = (uid => {
+  if (!uid) return null
+  try{
+    const userDocument =  firestore.collection('users').doc(uid).get();
+
+    return {uid, ...userDocument.data()}
+  } catch (error){
+    console.error('Error Fetching User', error.message);
+  }
+})
+
+export default app;
+
+
+
+  // Be wary of and figure out why this doesn't work
+//  const setting = {Timestamp: true}
+//  firestore.settings(setting);
+
+
